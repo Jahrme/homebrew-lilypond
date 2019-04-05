61c75e2204fff3fe133721f540c73498a91eb795
Jahrme Risner <github@jahrme.com>
2019-04-05 13:50:12 -0700

    Link optional texlive components

--- Formula/texlive-ly.rb
+++ Formula/texlive-ly.rb
@@ -5,15 +5,21 @@ class TexliveLy < Formula
   version "1.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
+  programs = [
+    "kpsewhich",
+    "mf",
+    "mf-nowin",
+    "mpost",
+    "bibtex",
+    "pdflatex",
+    "pdfetex",
+    "pdftex",
+    "etex",
+  ]
+
   def install
     bin.mkpath
     texbin = "/Library/TeX/texbin"
-    programs = [
-      "kpsewhich",
-      "mf",
-      "mf-nowin",
-      "mpost",
-    ]
     programs.each do |prog|
       ln_sf "#{texbin}/#{prog}", "#{bin}/#{prog}"
     end
@@ -21,24 +27,21 @@ class TexliveLy < Formula
 
   def caveats; <<-EOS
     Lilypond requires updated components from texlive provided by MacTex.
-    MacTeX can be installed using homebrew:
+    Some components of MacTeX depend on X11 which is provided by XQuartz.
 
-      brew cask install mactex-no-gui
+    MacTeX (and XQuartz) can be installed using homebrew cask:
 
-    After installation, the texlive componens must be updated:
+      brew cask install xquartz mactex-no-gui
+
+    After installation, the texlive components must be updated:
 
       sudo tlmgr update --self && sudo tlmgr update --all
-
-    Some components of MacTeX depend on X11 which is available through homebrew:
-
-      brew cask install xquartz
   EOS
   end
 
   test do
-    system "#{bin}/kpsewhich", "--version"
-    system "#{bin}/mf", "--version"
-    system "#{bin}/mf-nowin", "--version"
-    system "#{bin}/mpost", "--version"
+    programs.each do |prog|
+      system "#{bin}/#{prog}", "--version"
+    end
   end
 end
